da2dc11a3b77653b9c5802a87036a216f8eb16b9
CDoncecchi <77921161+CDoncecchi@users.noreply.github.com>
2021-01-31 11:24:06 +0100

    Update Etat.java

--- src/model/Etat.java
+++ src/model/Etat.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 import control.Control;
 import view.Affichage;
 
-/**Classe chargée de représenter le modèle*/
+/**Classe chargÃ©e de reprÃ©senter le modÃ¨le*/
 
 public class Etat {
 	
@@ -29,17 +29,17 @@ public class Etat {
 		this.aff = new Affichage(this);
 	}
 	
-	/**Méthode renvoyant l'hauteur de l'ovale*/
+	/**MÃ©thode renvoyant l'hauteur de l'ovale*/
 	public int getHauteur() {
 		return this.hauteur;
 	}
 	
-	/**méthode de mofification du modèle*/
+	/**mÃ©thode de mofification du modÃ¨le*/
 	public void jump() {
-		/**On vérifie que le joueur n'a paas perdu*/
+		/**On vÃ©rifie que le joueur n'a paas perdu*/
 		if(this.continuer) {
 			int nv = this.getHauteur()-Control.SAUT;
-			/**Vérifie que l'ovale ne sort pas du cadre*/
+			/**VÃ©rifie que l'ovale ne sort pas du cadre*/
 			if(nv>0) {
 				this.hauteur=nv;
 				this.testPerdu();
@@ -47,7 +47,7 @@ public class Etat {
 		}
 	}
 	
-	/**Méthode qui permet de modifier l'altitude de l'ovale*/
+	/**MÃ©thode qui permet de modifier l'altitude de l'ovale*/
 	public void moveDown() {
 		int nv = this.getHauteur()+Control.SAUT;
 		/**Verfie que l'ovale ne sorte pas du cadre*/
@@ -57,21 +57,21 @@ public class Etat {
 		}
 	}
 	
-	/**Méthode qui permet de savoir si le joueur est perdu*/
+	/**MÃ©thode qui permet de savoir si le joueur est perdu*/
 	public boolean testPerdu() {
-		/**Point à gauche de l'ovale pour le calcul de la pente*/
+	/**Point Ã  gauche de l'ovale pour le calcul de la pente*/
         Point p1 = this.parcours.getParcours().get(0);
-        /**Point à droite de l'ovale pour le calcul de la pente*/
+        /**Point Ã  droite de l'ovale pour le calcul de la pente*/
         Point p2 = this.parcours.getParcours().get(1);
         /**Calcul de la pente*/
         float pente = ((p2.y) - (p1.y)) / ((float)p2.x - (float)p1.x);
-        /**Ordonnée de l'ovale sur la pente*/
+        /**OrdonnÃ©e de l'ovale sur la pente*/
         float y = p1.y - pente*(p1.x-(Affichage.X+(Affichage.LARG_OVAL/2)));
-        /**Ordonnée min de l'ovale*/
+        /**OrdonnÃ©e min de l'ovale*/
         int yMin_ovale = this.hauteur;
-        /**Ordonnée max de l'ovale*/
+        /**OrdonnÃ©e max de l'ovale*/
         int yMax_ovale = this.hauteur + (Affichage.LARG_HAUT);
-        /**On vérifie que le joueur n'a pas perdu grâce de l'encadrement des valeurs max et min*/
+        /**On vÃ©rifie que le joueur n'a pas perdu grÃ¢ce de l'encadrement des valeurs max et min*/
         if(y >= yMin_ovale && y <= yMax_ovale) {
             return false;
         } else {
